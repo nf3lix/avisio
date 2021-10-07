92dbb3a5a8f9e4df71c49ea9e70d625660caa254
set <felix01.schneider@gmail.com>
2021-10-07 14:36:22 +0200

    Add box repository

--- dashboard/src/main/java/com/avisio/dashboard/common/persistence/AvisioBoxDao.kt
+++ dashboard/src/main/java/com/avisio/dashboard/common/persistence/AvisioBoxDao.kt
@@ -1,5 +1,6 @@
 package com.avisio.dashboard.common.persistence
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -10,7 +11,7 @@ import com.avisio.dashboard.common.data.model.AvisioBox
 interface AvisioBoxDao {
 
     @Query("SELECT * FROM box")
-    fun getBoxList(): List<AvisioBox>
+    fun getBoxList(): LiveData<List<AvisioBox>>
 
     @Insert
     fun insertBox(box: AvisioBox)
---
+++ dashboard/src/main/java/com/avisio/dashboard/common/persistence/AvisioBoxRepository.kt
@@ -0,0 +1,31 @@
+package com.avisio.dashboard.common.persistence
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.avisio.dashboard.common.data.database.AppDatabase
+import com.avisio.dashboard.common.data.model.AvisioBox
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class AvisioBoxRepository(application: Application) {
+
+    private val dao: AvisioBoxDao
+    private val database: AppDatabase = AppDatabase(application)
+    private val boxList: LiveData<List<AvisioBox>>
+
+    init {
+        dao = database.boxDao()
+        boxList = dao.getBoxList()
+    }
+
+    fun getBoxList(): LiveData<List<AvisioBox>> {
+        return boxList
+    }
+
+    fun insert(box: AvisioBox) {
+        GlobalScope.launch {
+            dao.insertBox(box)
+        }
+    }
+
+}
