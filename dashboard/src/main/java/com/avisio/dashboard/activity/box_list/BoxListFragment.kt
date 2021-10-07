e565920eb45ecee604115bde5a7c22a41518e379
set <felix01.schneider@gmail.com>
2021-10-07 17:08:26 +0200

    Move fab from main activity to box list fragment

--- dashboard/src/main/java/com/avisio/dashboard/activity/box_list/BoxListFragment.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/box_list/BoxListFragment.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.AvisioBoxViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class BoxListFragment : Fragment() {
 
@@ -17,17 +19,18 @@ class BoxListFragment : Fragment() {
     private lateinit var boxAdapter: AvisioBoxListAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.box_list_fragment, container, false)
     }
 
     override fun onStart() {
         super.onStart()
-        setupBoxList()
+        setupView()
     }
 
-    private fun setupBoxList() {
+    private fun setupView() {
         setupRecyclerView()
         setupBoxViewModel()
+        setupFab()
     }
 
     private fun setupRecyclerView() {
@@ -41,6 +44,13 @@ class BoxListFragment : Fragment() {
         boxViewModel = ViewModelProvider(this).get(AvisioBoxViewModel::class.java)
         boxViewModel.getBoxList().observe(this) { boxList ->
             boxAdapter.submitList(boxList)
+        }
+    }
+
+    private fun setupFab() {
+        view?.findViewById<FloatingActionButton>(R.id.fab)?.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
