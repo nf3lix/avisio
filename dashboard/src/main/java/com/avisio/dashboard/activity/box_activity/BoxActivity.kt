c78691d7d8274e63286dd979c65029dcd1bc6727
set <felix01.schneider@gmail.com>
2021-10-09 16:38:37 +0200

    Add options menu to BoxActivity

--- dashboard/src/main/java/com/avisio/dashboard/activity/box_activity/BoxActivity.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/box_activity/BoxActivity.kt
@@ -1,7 +1,9 @@
 package com.avisio.dashboard.activity.box_activity
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.ParcelableAvisioBox
 
 class BoxActivity : AppCompatActivity() {
@@ -12,6 +14,11 @@ class BoxActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         parcelableBox = intent.getParcelableExtra("BOX_OBJECT")!!
         supportActionBar?.title = parcelableBox.boxName
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.box_activity_menu, menu)
+        return true
     }
 
 }
