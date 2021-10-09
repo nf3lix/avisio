f66b9fcd2ba2b6b7a1006f99d802beafb6feee14
set <felix01.schneider@gmail.com>
2021-10-09 17:42:17 +0200

    Set correct box name input in EditBoxActivity

--- dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxActivity.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxActivity.kt
@@ -2,18 +2,24 @@ package com.avisio.dashboard.activity.edit_box
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.avisio.dashboard.R
+import com.avisio.dashboard.activity.box_activity.BoxActivity
+import com.avisio.dashboard.common.data.model.ParcelableAvisioBox
 
 class EditBoxActivity : AppCompatActivity() {
+
+    private lateinit var parcelableBox: ParcelableAvisioBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_box)
+        parcelableBox = intent.getParcelableExtra(BoxActivity.PARCELABLE_BOX_KEY)!!
         fillBoxInformation()
     }
 
     private fun fillBoxInformation() {
-
+        findViewById<EditText>(R.id.box_name_input).setText(parcelableBox.boxName)
     }
 
 }
