dbd314a93c378b722f924d2ebcefa45986e48755
felix <felix01.schneider@gmail.com>
2021-10-14 11:23:13 +0200

    Add fragment mode to EditBoxFragment

--- dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxFragment.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxFragment.kt
@@ -9,15 +9,18 @@ import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.ParcelableAvisioBox
 
 private const val BOX_OBJECT_KEY = "BOX_OBJECT_KEY"
+private const val FRAGMENT_MODE_KEY = "FRAGMENT_MODE_KEY"
 
 class EditBoxFragment : Fragment() {
 
     private lateinit var parcelableBox: ParcelableAvisioBox
+    private var fragmentMode: EditBoxFragmentMode = EditBoxFragmentMode.CREATE_BOX
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             parcelableBox = it.getParcelable(BOX_OBJECT_KEY)!!
+            fragmentMode = EditBoxFragmentMode.values()[it.getInt(FRAGMENT_MODE_KEY)]
         }
     }
 
@@ -28,10 +31,11 @@ class EditBoxFragment : Fragment() {
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(parcelableBox: ParcelableAvisioBox, fragmentMode: EditBoxFragmentMode) =
             EditBoxFragment().apply {
                 arguments = Bundle().apply {
-                    putString(BOX_OBJECT_KEY, param1)
+                    putParcelable(BOX_OBJECT_KEY, parcelableBox)
+                    putInt(FRAGMENT_MODE_KEY, fragmentMode.ordinal)
                 }
             }
     }
---
+++ dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxFragmentMode.kt
@@ -0,0 +1,5 @@
+package com.avisio.dashboard.activity.edit_box
+
+enum class EditBoxFragmentMode {
+    CREATE_BOX, EDIT_BOX
+}
