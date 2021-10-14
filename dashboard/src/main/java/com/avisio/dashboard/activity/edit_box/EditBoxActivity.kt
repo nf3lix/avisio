ddbd24013ff24fa32b3e54f07f9a244c36f28c0a
set <felix01.schneider@gmail.com>
2021-10-14 14:13:57 +0200

    propagate parcelable box from list to EditBoxActivity

--- dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxActivity.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxActivity.kt
@@ -6,28 +6,30 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.avisio.dashboard.R
+import com.avisio.dashboard.activity.box_activity.BoxActivity
 import com.avisio.dashboard.common.data.model.ParcelableAvisioBox
-import com.avisio.dashboard.common.persistence.AvisioBoxRepository
 
 class EditBoxActivity : AppCompatActivity() {
 
     private lateinit var parcelableBox: ParcelableAvisioBox
-    private lateinit var boxDao: AvisioBoxRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        parcelableBox = intent.getParcelableExtra(BoxActivity.PARCELABLE_BOX_KEY)!!
         setContentView(R.layout.activity_edit_box)
         if(savedInstanceState == null) {
-            val bundle = bundleOf(
-                EditBoxFragment.BOX_OBJECT_KEY to ParcelableAvisioBox(1, "TEST_NAME"),
-                EditBoxFragment.FRAGMENT_MODE_KEY to EditBoxFragmentMode.EDIT_BOX.ordinal)
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<EditBoxFragment>(R.id.fragment_container_view, args = bundle)
-            }
+            initFragment()
         }
     }
 
-
+    private fun initFragment() {
+        val bundle = bundleOf(
+            EditBoxFragment.BOX_OBJECT_KEY to parcelableBox,
+            EditBoxFragment.FRAGMENT_MODE_KEY to EditBoxFragmentMode.EDIT_BOX.ordinal)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<EditBoxFragment>(R.id.fragment_container_view, args = bundle)
+        }
+    }
 
 }
--- dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxFragment.kt
+++ dashboard/src/main/java/com/avisio/dashboard/activity/edit_box/EditBoxFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
 import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.ParcelableAvisioBox
 import com.avisio.dashboard.common.persistence.AvisioBoxRepository
