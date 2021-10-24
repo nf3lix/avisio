309d88a92ff9ca03eba44cef2573691eaa7b1144
felix <felix01.schneider@gmail.com>
2021-10-24 20:14:06 +0200

    Show warning on back pressed in EditCardFragment

--- dashboard/src/main/java/com/avisio/dashboard/common/ui/edit_card/EditCardFragment.kt
+++ dashboard/src/main/java/com/avisio/dashboard/common/ui/edit_card/EditCardFragment.kt
@@ -7,19 +7,45 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.activity.OnBackPressedCallback
 import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.card.CardType
+import com.avisio.dashboard.common.ui.ConfirmDialog
 
-class EditCardFragment : Fragment() {
+class EditCardFragment : Fragment(), ConfirmDialog.ConfirmDialogListener {
 
     override fun onStart() {
         super.onStart()
         view?.findViewById<Spinner>(R.id.card_type_spinner)!!.adapter =
             ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, CardType.values())
+        setOnBackPressedDispatcher()
+    }
+
+    private fun setOnBackPressedDispatcher() {
+        requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                onBackPressed()
+            }
+        })
+    }
+
+    fun onBackPressed() {
+        val dialog = ConfirmDialog<Boolean>(
+            requireContext(),
+            this,
+            getString(R.string.create_card_cancel_dialog_title),
+            getString(R.string.create_card_cancel_dialog_question)
+        )
+        dialog.showDialog(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_edit_card, container, false)
     }
+
+    override fun onConfirm(data: Any) {
+        activity?.finish()
+    }
+
 
 }
