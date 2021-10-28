c765e0ee793d0d6fb0bce5731b8ed4c79067918b
set <felix01.schneider@gmail.com>
2021-10-28 19:44:42 +0200

    add tests

--- dashboard/src/androidTest/java/com/avisio/dashboard/activity/box_activity/BoxActivityTest.kt
+++ dashboard/src/androidTest/java/com/avisio/dashboard/activity/box_activity/BoxActivityTest.kt
@@ -4,28 +4,25 @@ import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openContextualActionModeOverflowMenu
+import androidx.test.espresso.NoMatchingViewException
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.*
-import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.intent.matcher.IntentMatchers.*
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.avisio.dashboard.R
-import com.avisio.dashboard.activity.box_list.AvisioBoxViewHolder
 import com.avisio.dashboard.activity.crud_card.create_card.CreateCardActivity
 import com.avisio.dashboard.activity.crud_card.edit_card.EditCardActivity
+import com.avisio.dashboard.activity.edit_box.EditBoxActivity
 import com.avisio.dashboard.common.data.database.AppDatabase
 import com.avisio.dashboard.common.data.model.box.ParcelableAvisioBox
-import com.avisio.dashboard.common.data.model.card.Card
 import com.avisio.dashboard.common.persistence.CardDao
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
 class BoxActivityTest {
 
@@ -34,7 +31,7 @@ class BoxActivityTest {
     private lateinit var database: AppDatabase
 
     init {
-        val box = ParcelableAvisioBox(1, "BOX_NAME", 1)
+        val box = ParcelableAvisioBox(1, "BOX_NAME", R.drawable.box_icon_language)
         intent = Intent(ApplicationProvider.getApplicationContext(), BoxActivity::class.java)
         intent.putExtra(BoxActivity.PARCELABLE_BOX_KEY, box)
     }
@@ -55,6 +52,34 @@ class BoxActivityTest {
     @After
     fun releaseIntents() {
         release()
+    }
+
+    @Test
+    fun startCreateCardActivityOnFabClicked() {
+        onView(withId(R.id.fab_new_card)).perform(click())
+        intended(hasComponent(CreateCardActivity::class.java.name))
+    }
+
+    @Test
+    fun showDialogOnDeleteMenuItemClicked() {
+        openContextualActionModeOverflowMenu()
+        onView(withText(R.string.box_activity_menu_delete)).perform(click())
+        onView(withText(R.string.delete_box_confirm_dialog_message)).check(matches(isDisplayed()))
+    }
+
+    @Test(expected = NoMatchingViewException::class)
+    fun closeDeleteDialogOnCancelClicked() {
+        openContextualActionModeOverflowMenu()
+        onView(withText(R.string.box_activity_menu_delete)).perform(click())
+        onView(withText(R.string.confirm_dialog_cancel_default)).perform(click())
+        onView(withText(R.string.delete_box_confirm_dialog_message)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun startEditBoxActivityOnMenuItemSelected() {
+        openContextualActionModeOverflowMenu()
+        onView(withText(R.string.box_activity_menu_edit)).perform(click())
+        intended(hasComponent(EditBoxActivity::class.java.name))
     }
 
 }
