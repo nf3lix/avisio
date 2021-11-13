267272a89b809542833a6f4469480989acf7025b
set <felix01.schneider@gmail.com>
2021-11-13 19:30:20 +0100

    change CardInputInformation to data class

--- dashboard/src/main/java/com/avisio/dashboard/usecase/crud_card/common/input_flex_box/CardInputInformation.kt
+++ dashboard/src/main/java/com/avisio/dashboard/usecase/crud_card/common/input_flex_box/CardInputInformation.kt
@@ -1,6 +1,6 @@
 package com.avisio.dashboard.usecase.crud_card.common.input_flex_box
 
-class CardInputInformation(val message: String, val type: CardFlexBoxInformationType) {
+data class CardInputInformation(val message: String, val type: CardFlexBoxInformationType) {
 
     companion object {
         val NONE = CardInputInformation("", CardFlexBoxInformationType.BLANK)
@@ -8,24 +8,6 @@ class CardInputInformation(val message: String, val type: CardFlexBoxInformation
 
     fun isBlank(): Boolean {
         return this == NONE
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as CardInputInformation
-
-        if (message != other.message) return false
-        if (type != other.type) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = message.hashCode()
-        result = 31 * result + type.hashCode()
-        return result
     }
 
 }
