5f81015982f85dd7cbfcd35cfa6b4b0bfdec58f0
set <felix01.schneider@gmail.com>
2022-03-13 10:57:03 +0100

    Refactoring: early return in processing results of question image selection

--- dashboard/src/main/java/com/avisio/dashboard/usecase/crud_card/common/SelectQuestionImageResultObserver.kt
+++ dashboard/src/main/java/com/avisio/dashboard/usecase/crud_card/common/SelectQuestionImageResultObserver.kt
@@ -1,7 +1,6 @@
 package com.avisio.dashboard.usecase.crud_card.common
 
 import android.content.Intent
-import android.database.Cursor
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
@@ -10,7 +9,6 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import com.avisio.dashboard.common.persistence.card.CardImageStorage
 import java.io.File
@@ -30,19 +28,18 @@ class SelectQuestionImageResultObserver(
 
     override fun onCreate(owner: LifecycleOwner) {
         content = registry.register(OBSERVER_REGISTRY_KEY, ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-            if(result.data?.data != null) {
-                val path = getPathFromURI(result.data!!.data!!)
-                if(path != null) {
-                    val f = File(path)
-                    val selectedImageUri = Uri.fromFile(f)
-                    val drawable = Drawable.createFromPath(selectedImageUri.path!!)
-                    val bitmap = (drawable as BitmapDrawable).bitmap
-                    val fileName = UUID.randomUUID().toString()
-                    val imageStorage = CardImageStorage(editCardFragment.requireContext())
-                    imageStorage.saveBitmap(bitmap, fileName)
-                    editCardFragment.questionImageSelected(fileName)
-                }
+            if(result.data?.data == null) {
+                return@register
             }
+            val path = getPathFromURI(result.data!!.data!!) ?: return@register
+            val file = File(path)
+            val selectedImageUri = Uri.fromFile(file)
+            val drawable = Drawable.createFromPath(selectedImageUri.path!!)
+            val bitmap = (drawable as BitmapDrawable).bitmap
+            val fileName = UUID.randomUUID().toString()
+            val imageStorage = CardImageStorage(editCardFragment.requireContext())
+            imageStorage.saveBitmap(bitmap, fileName)
+            editCardFragment.questionImageSelected(fileName)
         }
     }
 
