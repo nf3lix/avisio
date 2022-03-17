6f4c34e5fd8bd1fc6b42fc638f936bd8cd3a4211
set <felix01.schneider@gmail.com>
2022-03-17 18:57:08 +0100

    Round corners of image in CardImage view

--- dashboard/src/main/java/com/avisio/dashboard/common/ui/card_image/CardImage.kt
+++ dashboard/src/main/java/com/avisio/dashboard/common/ui/card_image/CardImage.kt
@@ -3,13 +3,11 @@ package com.avisio.dashboard.common.ui.card_image
 import android.content.Context
 import android.graphics.Bitmap
 import android.util.AttributeSet
-import android.util.Log
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.avisio.dashboard.R
-import com.avisio.dashboard.common.ui.InputDialog
+import com.avisio.dashboard.common.ui.RoundedCornersBitmap
 import com.google.android.flexbox.FlexboxLayout
 import kotlinx.android.synthetic.main.card_image_view.view.*
 import kotlin.math.min
@@ -17,8 +15,12 @@ import kotlin.math.roundToInt
 
 class CardImage(context: Context, attributeSet: AttributeSet? = null) : ConstraintLayout(context, attributeSet) {
 
-    var maxHeight = 150.0
-    var maxWidth = 200.0
+    companion object {
+        const val IMAGE_CORNER_RADIUS = 40
+    }
+
+    private var maxHeight = 150.0
+    private var maxWidth = 200.0
 
     private var deleteImageClickListener: DeleteImageClickListener? = null
     private var currentBitmap: Bitmap? = null
@@ -31,12 +33,13 @@ class CardImage(context: Context, attributeSet: AttributeSet? = null) : Constrai
     }
 
     fun setImage(bitmap: Bitmap) {
+        val roundedBitmap = RoundedCornersBitmap.withRoundedCorners(bitmap, IMAGE_CORNER_RADIUS)
         val scale = min((maxHeight / bitmap.height), (maxWidth / bitmap.width))
         val params = FlexboxLayout.LayoutParams((bitmap.height * scale).roundToInt(), (bitmap.width * scale).roundToInt())
         params.isWrapBefore = true
         card_image_item.layoutParams = params
         card_image_item.scaleType = ImageView.ScaleType.CENTER_CROP
-        card_image_item.setImageBitmap(bitmap)
+        card_image_item.setImageBitmap(roundedBitmap)
         currentBitmap = bitmap
     }
 
