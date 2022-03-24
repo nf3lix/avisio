565a9fc05e1f5a02a03858f4abf26c817609ffa8
vn-vanessa <vanessa.vn@icloud.com>
2022-03-24 09:05:02 +0100

    add getCardCount (not working)

--- dashboard/src/main/java/com/avisio/dashboard/usecase/crud_box/common/BoxDetailActivity.kt
+++ dashboard/src/main/java/com/avisio/dashboard/usecase/crud_box/common/BoxDetailActivity.kt
@@ -7,12 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.avisio.dashboard.R
 import com.avisio.dashboard.common.data.model.box.AvisioBox
 import com.avisio.dashboard.common.data.transfer.getBoxObject
+import com.avisio.dashboard.common.persistence.card.CardRepository
 import kotlinx.android.synthetic.main.activity_box_detail.*
+import kotlinx.coroutines.runBlocking
 
 
 class BoxDetailActivity : AppCompatActivity() {
 
     private lateinit var box: AvisioBox
+    private val cardRepository = CardRepository(application)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +24,7 @@ class BoxDetailActivity : AppCompatActivity() {
         actionBarSetup()
 
         box_creation_date_detail_view_content.text = box.createDate.toString() //TODO Format ändern
-        box_card_count_detail_view_content.text = "TODO"
+        box_card_count_detail_view_content.text = "12" //getCardCount2().toString()
         box_card_open_detail_view_content.text = "TODO"
     }
 
@@ -31,5 +34,9 @@ class BoxDetailActivity : AppCompatActivity() {
             val actionBar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.box_detail_app_bar)
             actionBar?.title = box.name
         }
+    }
+
+    private fun getCardCount2(): Int = runBlocking {
+        return@runBlocking cardRepository.getCardsByBox(box.id).count()
     }
 }
